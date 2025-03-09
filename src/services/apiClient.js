a3abcf9cb1c4e00c81edb49aa95ae4a46f7f40f0
Vivek Lone <79907341+loliet-L@users.noreply.github.com>
2025-03-09 13:00:18 +0530

    commit-url

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL:  import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
