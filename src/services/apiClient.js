c5a6a85ce25c23a4f18f3322e14e4ebec71af186
Vivek Lone <79907341+loliet-L@users.noreply.github.com>
2025-03-09 13:09:01 +0530

    url-change

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL:  import.meta.env.VITE_API_URL,
-  timeout: 10000000,
+  baseURL: "https://backend-prod-gtc3.onrender.com/api",
+  timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
