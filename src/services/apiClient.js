b0bd9f8032a5aeb697f5b78911e1664dfe6d6ea7
Vivek Lone <79907341+loliet-L@users.noreply.github.com>
2025-03-09 13:13:15 +0530

    final commit

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
   baseURL: "https://backend-prod-gtc3.onrender.com/api",
-  timeout: 10000,
+  timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
